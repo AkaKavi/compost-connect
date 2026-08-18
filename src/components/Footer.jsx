97d74bc5fe91f82d71743ad9cda459513a92c14c
Kavyansh Nirwan <153480650+AkaKavi@users.noreply.github.com>
2026-08-18 23:27:41 +0530

    Fix copyright year display in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -93,7 +93,7 @@ export default function Footer() {
         {/* Copyright */}
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-500">
-          © {new Date().getFullYear()} Compost Connect
+           {new Date().getFullYear()} Compost Connect
         </div>
 
       </div>
