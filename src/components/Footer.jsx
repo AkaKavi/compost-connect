43f260484fdcedc539bca1dbd51acd675025c15f
Kavyansh Nirwan <153480650+AkaKavi@users.noreply.github.com>
2026-08-18 23:30:02 +0530

    Remove Home link from Footer component
    Removed the Home link from the footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,15 +31,6 @@ export default function Footer() {
 
               <li>
                 <Link
-                  to="/"
-                  className="hover:text-green-400 transition"
-                >
-                  Home
-                </Link>
-              </li>
-
-              <li>
-                <Link
                   to="/about"
                   className="hover:text-green-400 transition"
                 >
